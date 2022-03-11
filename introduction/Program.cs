e709e2536a496eaab3122a52e89e66a240f99fc4
David Badalyan <davit.badalyan.00@gmail.com>
2022-03-11 12:46:34 +0400

    Initial Commit

---
+++ introduction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace introduction
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# examples");
+        }
+    }
+}
