90a04a36ad8357cf3b986156d9a166d2f2d77155
David Badalyan <davit.badalyan.00@gmail.com>
2022-03-24 22:28:12 +0400

    nested-types

---
+++ nested-types/SubTopLevel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace nestedtypes
+{
+    public class SubTopLevel : TopLevel
+    {
+        //  Referring to a nested type from outside the enclosing type
+        private TopLevel.PublicNested nested;
+
+        public SubTopLevel()
+        {
+            //
+        }
+
+        //  Applying to protected access modifier to a nested type
+        public static void Foo()
+        {
+            new TopLevel.ProtectedNested();
+        }
+    }
+}
---
+++ nested-types/TopLevel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace nestedtypes
+{
+    public class TopLevel
+    {
+        public static int topLevelVariable;     //  Static variable
+        public enum Color { Red, Blue, Green};  //  Nested enum
+
+        public TopLevel()
+        {
+            //
+        }
+
+        //  Public nested class
+        public class PublicNested
+        {
+            //
+        }
+
+        //  Private nested class
+        private class PrivateNested {
+            //  Accessing a member of a type from nested type
+            static void Foo()
+            {
+                Console.WriteLine("topLevelVariable : " + TopLevel.topLevelVariable);
+            }
+        }
+
+        //  Protected nested class
+        protected class ProtectedNested
+        {
+            //
+        }
+    }
+}
